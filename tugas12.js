642596231a6688ca8c86d16df4dd96ab83e62fef
R-devMaker <rei.forworkonly@gmail.com>
2020-02-03 14:08:11 +0700

    Tugas 12 JavaScript
    Pengumpulan tugas 12 JavaScript Niomic

---
+++ tugas12.js
@@ -0,0 +1,11 @@
+function tugas12() {
+    var data = ['pisang','jeruk','apel','mangga'];
+    console.log(data);
+    data.pop();
+    console.log(data);
+    data.shift();
+    
+    return data;
+}
+
+console.log(tugas12());
